0c7aaede04c7887dae75d2e7ff2c70a73c98367d
Miguel Figueiredo <miguelprimavera76@gmail.com>
2024-03-03 03:30:04 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+// App.jsx
 import React from "react";
-import { Router, Route, Navigate, Router } from "react-router-dom";
+import { BrowserRouter as  Router, Routes, Route, Navigate} from "react-router-dom";
 import RegisterPage from "./components/RegisterPage.js";
 import LoginPage from "./components/LoginPage.js";
 import HomePage from "./components/HomePage.js";
 
+
+
 const App = () => {
   return (
     <Router>
-    <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+
       <Route path="*" element={<Navigate to="/login" />} />
       </Router>
   );
