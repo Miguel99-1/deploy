cec93294cbcb8509c588fbb70ddd041e13814cd5
Miguel Figueiredo <miguelprimavera76@gmail.com>
2024-03-04 08:37:48 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.jsx
 import React from "react";
-import { BrowserRouter as  Router, Route, Navigate} from "react-router-dom";
+import { BrowserRouter as  Router, Routes, Route, Navigate} from "react-router-dom";
 import RegisterPage from "./components/RegisterPage.js";
 import LoginPage from "./components/LoginPage.js";
 import HomePage from "./components/HomePage.js";
@@ -9,14 +9,36 @@ import HomePage from "./components/HomePage.js";
 
 const App = () => {
   return (
-    <Router>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-
-      <Route path="*" element={<Navigate to="/login" />} />
+      <Router>
+        {user && <Navbar user={user} onLogout={handleLogout} />}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/api" element={<ApiPage />} />
+          {user && (
+            <>
+              <Route path="/teams" element={<TeamsPage />} />
+              <Route path="/season-games" element={<SeasonGamesPage />} />
+              <Route path="/day-games" element={<DayGamesPage />} />
+              <Route path="/players" element={<PlayersPage />} />
+              <Route path="/standings" element={<StandingsPage />} />
+            </>
+          )}
+          <Route
+            path="/login"
+            element={
+              user ? <Navigate to="/" replace /> : <LoginPage onLogin={handleLogin} />
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              user ? <Navigate to="/" replace /> : <RegisterPage onRegister={handleRegister} />
+            }
+          />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
       </Router>
-  );
-}
+    );
+  };
 
 export default App;
