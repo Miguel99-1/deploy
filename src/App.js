ee62be9e4c994e8bc22c1c0b65340334c1b1cb9d
Miguel Figueiredo <miguelprimavera76@gmail.com>
2024-03-04 08:40:44 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -10,29 +10,19 @@ import HomePage from "./components/HomePage.js";
 const App = () => {
   return (
       <Router>
-        {user && <Navbar user={user} onLogout={handleLogout} />}
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/api" element={<ApiPage />} />
-          {user && (
+          { (
             <>
-              <Route path="/teams" element={<TeamsPage />} />
-              <Route path="/season-games" element={<SeasonGamesPage />} />
-              <Route path="/day-games" element={<DayGamesPage />} />
-              <Route path="/players" element={<PlayersPage />} />
-              <Route path="/standings" element={<StandingsPage />} />
             </>
           )}
           <Route
             path="/login"
-            element={
-              user ? <Navigate to="/" replace /> : <LoginPage onLogin={handleLogin} />
-            }
+            element={<LoginPage/>}
           />
           <Route
             path="/register"
-            element={
-              user ? <Navigate to="/" replace /> : <RegisterPage onRegister={handleRegister} />
+            element={ <RegisterPage/>
             }
           />
           <Route path="*" element={<Navigate to="/login" />} />
