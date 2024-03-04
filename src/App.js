83660f7641820d88489370d1f78e52a1b3e57f69
Miguel Figueiredo <miguelprimavera76@gmail.com>
2024-03-04 09:04:06 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
   return (
     <div className="app">
       <div className="screen">
-        <img src={Image}/>
+        <img src={Image} alt="img" />
         <h1>Register</h1>
 
         <label>
