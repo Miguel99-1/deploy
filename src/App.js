3f6ecc966f3d846e96dd944246041d0525f0f485
Miguel Figueiredo <miguelprimavera76@gmail.com>
2024-03-04 08:59:58 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,92 @@
-// App.jsx
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import RegisterPage from "./components/RegisterPage.js";
-import LoginPage from "./components/LoginPage.js";
-import HomePage from "./components/HomePage.js";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-const App = () => {
+import "./App.css";
+import Image from "./assets/image.svg";
+import Trash from "./assets/trash.svg";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const baseUrl = "https://api-register-users-1ype.vercel.app";
+
+  const getUsers = async () => {
+    const { data } = await axios.get(`${baseUrl}/users`);
+    setUsers(data);
+  };
+
+  const addNewUser = async () => {
+    if (!name || !email) {
+      return alert("Preencha os campos !");
+    }
+    const data = { name, email };
+
+    const { data: newUser } = await axios.post(`${baseUrl}/users`, data);
+
+    setUsers([...users, newUser]);
+    setName("");
+    setEmail("");
+  };
+
+  const removeUser = async (id) => {
+    await axios.delete(`${baseUrl}/users/${id}`);
+
+    setUsers(users.filter((user) => user.id !== id));
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>
-    </Router>
+    <div className="app">
+      <div className="screen">
+        <img src={Image} alt="image" />
+        <h1>Register</h1>
+
+        <label>
+          Name
+          <input
+            placeholder="Pedro Silva"
+            type="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Email
+          <input
+            placeholder="email@email.com"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+
+        <button onClick={addNewUser}>Register new user</button>
+
+        <ul>
+          {users.map((user) => (
+            <li>
+              <div>
+                <p>{user?.name}</p>
+                <p>{user?.email}</p>
+              </div>
+              <button
+                className="btn-trash"
+                onClick={() => removeUser(user?.id)}
+              >
+                <img src={Trash} alt="trash" />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
-};
+}
 
 export default App;
