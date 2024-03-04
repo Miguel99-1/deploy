46b79da874d3345ef54b78f534c9bd67761a4a22
Miguel Figueiredo <miguelprimavera76@gmail.com>
2024-03-04 08:48:41 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 // App.jsx
 import React from "react";
-import { BrowserRouter as  Routes, Route, Navigate} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import RegisterPage from "./components/RegisterPage.js";
 import LoginPage from "./components/LoginPage.js";
 import HomePage from "./components/HomePage.js";
 
-
-
 const App = () => {
   return (
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route
-            path="/login"
-            element={<LoginPage/>}
-          />
-          <Route
-            path="/register"
-            element={ <RegisterPage/>
-            }
-          />
-          <Route path="*" element={<Navigate to="/login" />} />
-        </Routes>
-    );
-  };
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
